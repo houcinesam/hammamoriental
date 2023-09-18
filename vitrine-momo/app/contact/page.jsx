a654d62ff410f27c49a450036487a630f2e57fe4
david abruzzo <david.abruzzo@gmail.com>
2023-09-18 07:05:24 +0200

    added formspree

--- vitrine-momo/app/contact/page.jsx
+++ vitrine-momo/app/contact/page.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+"use client";
+import ContactForm from "../../components/ContactForm";
 
-function page() {
+const page = () => {
   return (
-    <div className="text-3xl flex justify-center items-center w-full h-screen">
-      contact
+    <div className="flex flex-row items-center justify-around">
+      <ContactForm />
+      <div className="flex flex-col">
+        <p>vous pouvez egalement nous contacter:</p>
+        <p> par telephone 06.06.06.06.06</p>
+        par courrier : 28 rue du bout de la rue
+      </div>
     </div>
   );
-}
-
+};
 export default page;
---
+++ vitrine-momo/components/ContactForm.jsx
@@ -0,0 +1,85 @@
+import { useForm, ValidationError } from '@formspree/react';
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xknlbpbv");
+
+  if (state.succeeded) {
+    return <p>Merci de votre message, nous vous recontacterons au plus vite.</p>;
+  }
+
+  return (
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold mb-4">Contactez-nous</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2">
+              Prénom
+            </label>
+            <input
+              id="firstName"
+              type="text"
+              name="firstName"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block text-gray-700 font-bold mb-2">
+              Nom de famille
+            </label>
+            <input
+              id="lastName"
+              type="text"
+              name="lastName"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+              Adresse e-mail
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+            />
+            <ValidationError prefix="Email" field="email" errors={state.errors} />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">
+              Téléphone
+            </label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full px-3 py-2 border rounded-lg resize-none focus:outline-none focus:ring focus:border-blue-500"
+              rows="4"
+            />
+            <ValidationError prefix="Message" field="message" errors={state.errors} />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            disabled={state.submitting}
+          >
+            Envoyer
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactForm;
