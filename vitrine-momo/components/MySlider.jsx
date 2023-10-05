7da42052eb36abb12b2d42d4c48a5aa7a12e27ac
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-10-05 04:37:08 +0200

    revision

--- vitrine-momo/components/MySlider.jsx
+++ vitrine-momo/components/MySlider.jsx
@@ -4,9 +4,12 @@ import KeenSlider from "keen-slider";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
-
 export default () => {
-  const photos = ["https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png", "https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png", "https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png"];
+  const photos = [
+    "https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png",
+    "https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png",
+    "https://img.itch.zone/aW1nLzEzNDI2NTA3LnBuZw==/315x250%23c/rFdj2T.png",
+  ];
   const [sliderRef, instanceRef] = useKeenSlider(
     {
       slideChanged() {
@@ -19,14 +22,16 @@ export default () => {
   );
 
   return (
-    <div ref={sliderRef} className="keen-slider ">
-      {photos.map((photo, index) => {
-        return (
-          <div className="keen-slider__slide number-slide" key={index}>
-             <img src={photo} alt={`Image ${index}`} />
-          </div>
-        );
-      })}
+    <div className="justify-center ">
+      <div ref={sliderRef} className="keen-slider ">
+        {photos.map((photo, index) => {
+          return (
+            <div className="keen-slider__slide number-slide" key={index}>
+              <img src={photo} alt={`Image ${index}`} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- vitrine-momo/components/navigation/navbar/index.jsx
+++ vitrine-momo/components/navigation/navbar/index.jsx
@@ -3,34 +3,36 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header>  
-    <div className="bg-gray-300 text-2xl md:3xl flex flex-row vw-full justify-between mb-10">
-      <div className="display flex items-center justify-between ">
+    <header> 
+      <div className="bg-white"> 
+    <div className="text-2xl md:3xl flex flex-row vw-full justify-between ">
+      <div className="display flex flex-col justify-between ">
         <img
           className="h-auto md:w-64"
-          src="/MomoLeBricolo.png"
-          alt="logo de momo le bricolo"
+          src="/carteVisiteMomoLogo-removebg-preview.png"
+          alt="logo de Multiservice pro"
         />
+       
       </div>
-      <div className=" flex-row  vw-[100%] justify-between items-center mr-[10%] md:mt-[1%] mb-[1%]">
-        <ul className="  flex flex-col md:flex-row md:space-x-4 text-2xl md:text-3xl  mt-2 md:mt-6 justify-around ">
-          <li className="block px-4 py-2 rounded-md border border-blue-500 shadow-inner-xl md:bg-transparent hover:bg-gray-200">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="block px-4 py-2 rounded-md  border border-blue-500 md:bg-transparent hover:bg-gray-200">
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li className="block px-4 py-2 rounded-md border border-blue-500 md:bg-transparent hover:bg-gray-200">
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li className="block px-4 py-2 rounded-md border border-blue-500 md:bg-transparent hover:bg-gray-200">
-            <Link href="/gallery">Photos</Link>
-          </li>
-        </ul>
-        <h2 className="hidden md:text-xl md:flex md:flex-col md:items-center md:justify-center  transition-transform transform hover:scale-150 mt-10 hover:border border-gray-600 hover:rounded-mdd p-2">
-          Pour tout vos travaux autour de Hombourg-Haut
-        </h2>
+      <div className=" flex-row  vw-[100%] justify-around items-center  md:mt-[1%]  ">
+      <ul className="flex flex-col md:flex-row md:space-x-4 text-2xl md:text-3xl md:mt-6 justify-around text-white h-full">
+  <li className="flex-grow block px-4 py-2 rounded-md border shadow-inner-xl bg-[#484848] hover:bg-[#f79b20]">
+    <Link href="/">Accueil</Link>
+  </li>
+  <li className="flex-grow block px-4 py-2 rounded-md border  bg-[#484848] hover:bg-[#f79b20]">
+    <Link href="/contact">Contact</Link>
+  </li>
+  <li className="flex-grow block px-4 py-2 rounded-md border m-[1%] bg-[#484848] hover:bg-[#f79b20]">
+    <Link href="/dashboard">Realisations</Link>
+  </li>
+  <li className="flex-grow block px-4 py-2 rounded-md border bg-[#484848] hover:bg-[#f79b20]">
+    <Link href="/gallery">Photos</Link>
+  </li>
+</ul>
+
+        
       </div>
+    </div>
     </div>
     </header>
   );
