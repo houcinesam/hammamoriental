5666d515ac2c6235137e178ded6ae4ccfb264c97
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-10-06 18:14:30 +0200

    fix

--- vitrine-momo/app/page.jsx
+++ vitrine-momo/app/page.jsx
@@ -11,11 +11,11 @@ function Home() {
           <h2 className="text-2xl flex  w-full ml-8 text-center ">Auto-entrepreneur dans divers travaux et habitat</h2>
           <p className="h-1 w-full bg-neon mb-10"></p>
           <div className="flex flex-col  bg-[#484848] justify-center text-neon text-2xl m-5  shadow-lg shadow-neon rounded-2xl ">
-            <p className="ml-5 mt-5 hover:text-3xl  hover:rounded hover:shadow-md hover:shadow-white">Rafraichissement des murs</p>
-            <p className="ml-5 mt-5 hover:text-3xl  hover:rounded hover:shadow-md hover:shadow-white">Bricolage</p>
-            <p className="ml-5 mt-5 hover:text-3xl  hover:rounded hover:shadow-md hover:shadow-white">Reparations divers</p>
-            <p className="ml-5 mt-5 hover:text-3xl  hover:rounded hover:shadow-md hover:shadow-white">Montage des meubles</p>
-            <p className="ml-5 mt-5 mb-5 hover:text-3xl  hover:rounded hover:shadow-md hover:shadow-white">Renovation & creation</p>
+            <p className="ml-5 mt-5 mr-5   hover:rounded hover:shadow-md hover:shadow-white">Rafraichissement des murs</p>
+            <p className="ml-5 mt-5 mr-5   hover:rounded hover:shadow-md hover:shadow-white">Bricolage</p>
+            <p className="ml-5 mt-5 mr-5   hover:rounded hover:shadow-md hover:shadow-white">Reparations divers</p>
+            <p className="ml-5 mt-5 mr-5   hover:rounded hover:shadow-md hover:shadow-white">Montage des meubles</p>
+            <p className="ml-5 mt-5 mr-5 mb-5   hover:rounded hover:shadow-md hover:shadow-white">Renovation & creation</p>
           </div>
         </div>
       </div>
--- vitrine-momo/components/navigation/navbar/index.jsx
+++ vitrine-momo/components/navigation/navbar/index.jsx
@@ -1,39 +1,39 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <header> 
-      <div className="bg-white"> 
-    <div className="text-2xl md:3xl flex flex-row vw-full justify-between ">
-      <div className="display flex flex-col justify-between ">
-        <img
-          className="h-auto md:w-64"
-          src="/carteVisiteMomoLogo-removebg-preview.png"
-          alt="logo de Multiservice pro"
-        />
-       
+    <header>
+      <div className="bg-white">
+        <div className="text-2xl md:3xl flex flex-row vw-full justify-between ">
+          <div className="display flex flex-col justify-between justify-around">
+            <Image
+              // className="h-auto md:w-64"
+              src="/carteVisiteMomoLogo-removebg-preview.png"
+              alt="logo de Multiservice pro"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className=" flex-row  vw-[100%] justify-around items-center  md:mt-[1%]  ">
+            <ul className="flex flex-col md:flex-row md:space-x-4 text-2xl md:text-3xl md:mt-6 justify-around text-white h-full">
+              <li className="flex-grow block px-4 py-2 rounded-md border shadow-inner-xl bg-[#484848] hover:bg-[#f79b20]">
+                <Link href="/">Accueil</Link>
+              </li>
+              <li className="flex-grow block px-4 py-2 rounded-md border  bg-[#484848] hover:bg-[#f79b20]">
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li className="flex-grow block px-4 py-2 rounded-md border m-[1%] bg-[#484848] hover:bg-[#f79b20]">
+                <Link href="/dashboard">Realisations</Link>
+              </li>
+              <li className="flex-grow block px-4 py-2 rounded-md border bg-[#484848] hover:bg-[#f79b20]">
+                <Link href="/gallery">Photos</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className=" flex-row  vw-[100%] justify-around items-center  md:mt-[1%]  ">
-      <ul className="flex flex-col md:flex-row md:space-x-4 text-2xl md:text-3xl md:mt-6 justify-around text-white h-full">
-  <li className="flex-grow block px-4 py-2 rounded-md border shadow-inner-xl bg-[#484848] hover:bg-[#f79b20]">
-    <Link href="/">Accueil</Link>
-  </li>
-  <li className="flex-grow block px-4 py-2 rounded-md border  bg-[#484848] hover:bg-[#f79b20]">
-    <Link href="/contact">Contact</Link>
-  </li>
-  <li className="flex-grow block px-4 py-2 rounded-md border m-[1%] bg-[#484848] hover:bg-[#f79b20]">
-    <Link href="/dashboard">Realisations</Link>
-  </li>
-  <li className="flex-grow block px-4 py-2 rounded-md border bg-[#484848] hover:bg-[#f79b20]">
-    <Link href="/gallery">Photos</Link>
-  </li>
-</ul>
-
-        
-      </div>
-    </div>
-    </div>
     </header>
   );
 };
