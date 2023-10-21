a920af595bb5838c33d83c8c3dcef92c31f29209
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-10-21 06:55:25 +0200

    added captcha

--- vitrine-momo/components/ContactForm.jsx
+++ vitrine-momo/components/ContactForm.jsx
@@ -1,14 +1,14 @@
-import { useForm, ValidationError } from '@formspree/react';
-;
-
+import { useForm, ValidationError } from "@formspree/react";
+import ReCAPTCHA from "react-recaptcha";
 
 function ContactForm() {
-  
   const key = process.env.NEXT_PUBLIC_FORMSPREE;
   const [state, handleSubmit] = useForm(key);
 
   if (state.succeeded) {
-    return <p>Merci de votre message, nous vous recontacterons au plus vite.</p>;
+    return (
+      <p>Merci de votre message, nous vous recontacterons au plus vite.</p>
+    );
   }
 
   return (
@@ -16,14 +16,15 @@ function ContactForm() {
       <div className="h-4 bg-[#f79b20]"></div>
       <div className=" p-8 rounded-lg shadow-md ">
         <h1 className="text-2xl font-semibold mb-4">Contactez-nous : </h1>
-        <p className="mb-2 text-xl">
-          &#9990; : 07 52 06 31 20 
-        </p>
-        <p className='text-xl'>Ou avec le formulaire :</p>
+        <p className="mb-2 text-xl">&#9990; : 07 52 06 31 20</p>
+        <p className="text-xl">Ou avec le formulaire :</p>
         <div className="h-1 bg-[#f79b20]"></div>
         <form onSubmit={handleSubmit}>
           <div className="mb-4 ">
-            <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2 ">
+            <label
+              htmlFor="firstName"
+              className="block text-gray-700 font-bold mb-2 "
+            >
               Prénom
             </label>
             <input
@@ -32,11 +33,19 @@ function ContactForm() {
               name="firstName"
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             />
+            <ValidationError
+              prefix="firstName"
+              field="firstName"
+              errors={state.errors}
+            />
           </div>
           <div className="h-1 bg-[#f79b20]"></div>
           <div className="mb-4">
-            <label htmlFor="lastName" className="block text-gray-700 font-bold mb-2">
-              Nom 
+            <label
+              htmlFor="lastName"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Nom
             </label>
             <input
               id="lastName"
@@ -44,10 +53,19 @@ function ContactForm() {
               name="lastName"
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             />
+
+            <ValidationError
+              prefix="lastName"
+              field="lastName"
+              errors={state.errors}
+            />
           </div>
           <div className="h-1 bg-[#f79b20]"></div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Adresse e-mail
             </label>
             <input
@@ -56,11 +74,18 @@ function ContactForm() {
               name="email"
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             />
-            <ValidationError prefix="Email" field="email" errors={state.errors} />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
           <div className="h-1 bg-[#f79b20]"></div>
           <div className="mb-4">
-            <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="phone"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Téléphone
             </label>
             <input
@@ -70,9 +95,17 @@ function ContactForm() {
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             />
           </div>
+          <ValidationError 
+        prefix="phone" 
+        field="phone"
+        errors={state.errors}
+      />
           <div className="h-1 bg-[#f79b20]"></div>
           <div className="mb-4">
-            <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="message"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Message
             </label>
             <textarea
@@ -81,10 +114,21 @@ function ContactForm() {
               className="w-full px-3 py-2 border rounded-lg resize-none focus:outline-none focus:ring focus:border-blue-500"
               rows="4"
             />
-            <ValidationError prefix="Message" field="message" errors={state.errors} />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
           <div className="h-1 bg-[#f79b20]"></div>
-          <div class="g-recaptcha" data-sitekey="6LdvCLIoAAAAAECBx30E6wm4pDPTryBGJlPLkcoa"></div>
+          {/* <div
+            class="g-recaptcha"
+            data-sitekey="6LdvCLIoAAAAAECBx30E6wm4pDPTryBGJlPLkcoa"
+          ></div> */}
+         <ReCAPTCHA sitekey="6LdvCLIoAAAAAECBx30E6wm4pDPTryBGJlPLkcoa" />
+
+
+
           <button
             type="submit"
             className="bg-blue-500 text-white mt-2 py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
