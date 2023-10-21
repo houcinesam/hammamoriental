d3ca23f25ccf482e2fb6ba9ea483b9c17b4014e2
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-10-21 08:03:10 +0200

    decomment

--- vitrine-momo/app/layout.jsx
+++ vitrine-momo/app/layout.jsx
@@ -1,14 +1,14 @@
-// export const metadata = {
-//   title: "Multiservices Pro",
-//   description: "Création & Rénovation",
-//   name: "google-site-verification",
-//   content: "EsetRKUhqW3gIS_m9j9If0AEgjMnhZH_w2Rw_DTOYqY",
-//   og: {
-//     title: "Multiservice Pro",
-//     description: "Renovation & Rénovation",
-//   },
+export const metadata = {
+  title: "Multiservices Pro",
+  description: "Création & Rénovation",
+  name: "google-site-verification",
+  content: "EsetRKUhqW3gIS_m9j9If0AEgjMnhZH_w2Rw_DTOYqY",
+  og: {
+    title: "Multiservice Pro",
+    description: "Renovation & Rénovation",
+  },
   
-// };
+};
 import Layout from "@/components/Layout";
 export default function RootLayout({ children }) {
   return (
