c7c883790c4d0906454f8694f2835235a25683af
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-10-21 10:05:03 +0200

    echap problem

--- vitrine-momo/app/page.jsx
+++ vitrine-momo/app/page.jsx
@@ -38,32 +38,50 @@ function Home() {
             />{" "}
             <p className="m-5 w-5/6 text-left text-2xl  w-9/10 ml-5  text-[#484848] lg:text-3xl lg:ml-20 ">
               {" "}
-              {`Que vous souhaitiez rénover une pièce existante, créer un nouvel
-              espace ou redonner vie à vos murs, notre équipe d'experts est là
-              pour vous accompagner. Nous transformons vos idées en réalité.`}
+              {`Que vous souhaitiez rénover une pièce existante, créer un nouvel espace ou redonner vie à vos murs, notre équipe d&apos;experts est là pour vous accompagner. Nous transformons vos idées en réalité.`}
             </p>
           </div>
           <p className="flex  h-1 w-[90%] bg-neon mb-10 mt-5 mr-5 ml-5 shadow-neon "></p>
           <div className="flex flex-col     text-[#484848] text-2xl m-5 lg:ml-20 lg:text-3xl    ">
             <div className="flex flex-col md:flex-row justify-between  align-top text-[#484848] text-2xl m-5 lg:ml-20 lg:text-3xl    ">
               {" "}
-              <p className="ml-5  mr-5 w-5/6  align-text-top ">Rafraichissement des murs</p>
-              <p className="ml-5 md:ml-20">Laissez nos artistes du mur donner un nouveau souffle à votre intérieur. Peinture, papier peint, textures , nous faisons des murs une toile pour votre créativité.</p>
+              <p className="ml-5  mr-5 w-5/6  align-text-top ">
+                Rafraichissement des murs
+              </p>
+              <p className="ml-5 md:ml-20">
+                Laissez nos artistes du mur donner un nouveau souffle à votre
+                intérieur. Peinture, papier peint, textures , nous faisons des
+                murs une toile pour votre créativité.
+              </p>
             </div>
             <div className="flex flex-col md:flex-row  justify-between  text-[#484848] text-2xl m-5 lg:ml-20 lg:text-3xl    ">
               {" "}
               <p className="ml-5  mr-5  w-5/6   ">Bricolage</p>
-              <p className="ml-5 md:ml-20"> Nous sommes là pour les projets de bricolage que vous avez en tête. Des petites réparations aux projets plus complexes, nous sommes prêts à relever le défi.</p>
+              <p className="ml-5 md:ml-20">
+                {" "}
+                Nous sommes là pour les projets de bricolage que vous avez en
+                tête. Des petites réparations aux projets plus complexes, nous
+                sommes prêts à relever le défi.
+              </p>
             </div>
             <div className="flex flex-col md:flex-row   text-[#484848] text-2xl m-5 lg:ml-20 lg:text-3xl    ">
               {" "}
               <p className="ml-5  mr-5   w-5/6  ">Reparations diverses</p>
-              <p className="ml-5 md:ml-[100px]"> {`Les problèmes dans la maison ne sont pas un problème pour nous. Que ce soit la plomberie, l'électricité, la menuiserie ou autre, nous sommes là pour réparer et améliorer.`}</p>
+              <p className="ml-5 md:ml-[100px]">
+                {" "}
+                {`Les problèmes dans la maison ne sont pas un problème pour nous. Que ce soit la plomberie, l'électricité, la menuiserie ou autre, nous sommes là pour réparer et améliorer.`}
+
+              </p>
             </div>
             <div className="flex flex-col md:flex-row   text-[#484848] text-2xl m-5 lg:ml-20 lg:text-3xl    ">
               {" "}
               <p className="ml-5  mr-5   w-5/6  ">Montage des meubles</p>
-              <p className="ml-5 md:ml-[100px]"> Vous avez acheté des meubles en kit ? Laissez-nous les assembler de manière professionnelle, pour que vous puissiez profiter de votre nouvel ameublement sans tracas.</p>
+              <p className="ml-5 md:ml-[100px]">
+                {" "}
+                Vous avez acheté des meubles en kit ? Laissez-nous les assembler
+                de manière professionnelle, pour que vous puissiez profiter de
+                votre nouvel ameublement sans tracas.
+              </p>
             </div>
           </div>
           <div className=" flex flex-col justify-center items-center pt-4 pb-4 bg-[#484848] text-neon text-center text-3xl flex items-center md:text-5xl text-center">
