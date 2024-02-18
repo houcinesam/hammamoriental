f5bcf332546f1dba97ee0eb2978293f6dce2d050
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2024-02-18 10:23:25 +0100

    fixed background

--- vitrine-momo/app/contact/page.jsx
+++ vitrine-momo/app/contact/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 import ContactForm from "../../components/ContactForm";
-
+import "../globals.css";
+import Image from "next/image";
+import background from "@/public/backgroundImage.webp";
 const page = () => {
   return (
-    <div className="flex flex-col   md:flex-row">
+    <div className="flex flex-col  md:flex-row bg-cover bg-center" style={{ backgroundImage: `url(${background.src})` }} >
       <ContactForm />
        
     </div>
