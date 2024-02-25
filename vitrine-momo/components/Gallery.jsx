20f5415e55b02209962d0c4cf679e5b18f767f9e
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2024-02-25 10:13:14 +0100

    image fix

--- vitrine-momo/components/Gallery.jsx
+++ vitrine-momo/components/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = ({ images, imageSize }) => {
       {images.map((image, index) => (
         <div
           key={index}
-          className="h-auto md:w-64 lg:ml-20 mt-10 mb-10 border-8 rounded-3xl border-buttonColor transition-transform duration-300 transform hover:scale-110 hover:shadow-2xl"
+          className="h-auto md:w-64  mt-10 mb-10 border-8 rounded-3xl border-buttonColor transition-transform duration-300 transform hover:scale-110 hover:shadow-2xl"
         >
           <Image src={image.src} alt={image.alt} {...imageSize} />
         </div>
